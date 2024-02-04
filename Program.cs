63e4e026a0556701495100dac537b740abdbba46
Prakhar <prakhar2788@gmail.com>
2024-02-05 00:55:25 +0530

    [Prakhar] Add: A practice code for queue

--- Program.cs
+++ Program.cs
@@ -10,61 +10,29 @@ namespace Collection_Framework
     {
         static void Main(string[] args)
         {
-            List<string> listna = new List<string>();
+            Queue<string> saurabh = new Queue<string>();
 
-            listna.Add("A");
-            listna.Add("B");
-            listna.Add("C");
-            listna.Add("D");
-            listna.Add("E");
+            saurabh.Enqueue("Vinu");
+            saurabh.Enqueue("Prakhar");
+            saurabh.Enqueue("Shouvik");
+            saurabh.Enqueue("Das");
 
-            //Console.WriteLine(listna);
-            foreach (var i in listna)
+            foreach(string s in saurabh)
             {
-                Console.Write("     "+i);
+                Console.WriteLine(s);
             }
 
 
-            Console.WriteLine();
-            Console.WriteLine("\nRemoving B from the list\n");
-            listna.Remove("B");
+            Console.WriteLine("\n"+saurabh.Dequeue()+"\n");
 
-            foreach (var i in listna)
+            foreach (string s in saurabh)
             {
-                Console.Write("     " + i);
+                Console.WriteLine(s);
             }
 
-            Console.WriteLine("\nRemoving elements in the range 0 to 3\n");
-            listna.RemoveRange(0,3);
+            Console.WriteLine("\n" + saurabh.Peek());
 
-            foreach (var i in listna)
-            {
-                Console.Write("     " + i);
-            }
 
-            listna.Add("b");
-            listna.Add("c");
-            listna.Add("d");
-            listna.Add("e");
-            listna.Add("X");
-            Console.WriteLine("\n");
-            foreach (var i in listna)
-            {
-                Console.Write("     " + i);
-            }
-
-            Console.WriteLine("\n Elememt at 0th positon: "+ listna[0] + "\n");
-
-            Console.WriteLine("Index of X: " + listna.IndexOf("X"));
-
-            Console.WriteLine("\nList Count: "+ listna.Count+"\n");
-            
-
-            foreach(object i in listna)
-            {
-                Console.Write("     " + i);
-            }
-            Console.WriteLine();
         }
     }
 }
