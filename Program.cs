d6a3e606abe82dcb747b0861c9ce3c8d59a9b273
Prakhar <prakhar2788@gmail.com>
2024-02-05 02:51:36 +0530

    [Prakhar] Add: A practice code for stack

--- Program.cs
+++ Program.cs
@@ -10,29 +10,56 @@ namespace Collection_Framework
     {
         static void Main(string[] args)
         {
-            Queue<string> saurabh = new Queue<string>();
+            Stack<int> qwe = new Stack<int>();
 
-            saurabh.Enqueue("Vinu");
-            saurabh.Enqueue("Prakhar");
-            saurabh.Enqueue("Shouvik");
-            saurabh.Enqueue("Das");
+            qwe.Push(1);
+            qwe.Push(2);
+            qwe.Push(3);
+            qwe.Push(4);
+            qwe.Push(5);
+            qwe.Push(6);
+            qwe.Push(7);
+            qwe.Push(8);
+            qwe.Push(9);
+            qwe.Push(10);
+            qwe.Push(11);
+            qwe.Push(12);
 
-            foreach(string s in saurabh)
+            Console.WriteLine("Does it contain 4 inside it: "+qwe.Contains(4));
+
+            Console.WriteLine("\n Total Elements: " + qwe.Count);
+
+            Console.WriteLine("\n Popping: "+qwe.Pop());
+
+            Console.WriteLine("\n Total Elements: "+qwe.Count);
+
+            Console.WriteLine("\n Top Element: "+qwe.Peek());
+
+            Console.WriteLine();
+            foreach(int i in qwe)
             {
-                Console.WriteLine(s);
+                Console.Write(" "+i);
+            }
+
+            
+            Console.WriteLine();
+
+            int[] ints = qwe.ToArray();
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
             }
 
 
-            Console.WriteLine("\n"+saurabh.Dequeue()+"\n");
 
-            foreach (string s in saurabh)
+            qwe.Clear();
+            Console.WriteLine("\nElements are gone: ");
+            foreach (int i in qwe)
             {
-                Console.WriteLine(s);
+                Console.Write(" " + i);
             }
-
-            Console.WriteLine("\n" + saurabh.Peek());
-
-
+            Console.WriteLine();
         }
     }
 }
